5f7b4abe282f735e0e5e66beb8044d9aa30d7b73
swastik dwivedi <drunkcj@localhost.localdomain>
2023-08-14 20:00:25 +0530

    adding process.env to load environment from web service environment

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,12 +3,12 @@ import { DataSource } from "typeorm"
 import { Contact } from "../models/Contact"
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "nigga",
-    database: "BiteSpeed",
+    type:  'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'nigga',
+    database: process.env.DB_DATABASE || 'BiteSpeed',
     synchronize: true,
     logging: false,
     entities: [Contact],
